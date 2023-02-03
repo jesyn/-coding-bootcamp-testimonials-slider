5b0ec4a9d8c24ce116dcdae5a529943d93821825
Jesica Muñoz <jesydynathos@gmail.com>
2023-02-02 22:08:46 -0300

    proyecto completo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Testimonials />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
